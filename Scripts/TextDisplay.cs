f08d00c29277134f92eab2d5854e63b84dd88e23
Sam Whitehead <sam@whitehead.nl>
2025-10-09 22:30:19 +0100

    prep for coding tomorrow!!

--- Scripts/TextDisplay.cs
+++ Scripts/TextDisplay.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class TextDisplay : Node2D
 {
+    private RichTextLabel _mainText;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _mainText = GetNode<RichTextLabel>("Text");
+        _mainText.Text = "Hello!";
+    }
 }
