da8da817fd2c8a308c593ad0c3378bee33b4e566
Sam Whitehead <sam@whitehead.nl>
2025-10-10 12:59:52 +0100

    import onto laptop

--- .godot/mono/temp/obj/Debug/CosmicHorror.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/CosmicHorror.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fe56baed8f47822438b17311bdf33503644dce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e65c7c5d30ba01f95dd6e96704c868a794266e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
