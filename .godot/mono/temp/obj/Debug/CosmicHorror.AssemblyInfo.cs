84517bac48c67c532ffe28227f1b8bac7a380b4b
albielee <albertoarjanlee@gmail.com>
2025-10-11 17:30:34 +0100

    files

--- .godot/mono/temp/obj/Debug/CosmicHorror.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/CosmicHorror.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c0657db0e03ddedad1afd9472066c8bbe795c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63290ee10abb22112ffbf210dd99adc4d8dfefa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CosmicHorror")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
